7f000f76e3453eec207a4276adca7ba1de45cbb4
Adarsh <serviceid02@gmail.com>
2025-02-25 18:52:30 +0530

    bmt landing page design

---
+++ extra-not-for-live/Online__clg---list.rb
@@ -0,0 +1,25 @@
+Online:
+
+Lovely Professional University
+
+Manipal University Jaipur
+
+Sikkim Manipal University
+
+Amity University Online
+
+Jain University
+
+Chandigarh University
+
+Sharda University
+
+Uttaranchal University
+
+DPU
+
+Amity Jaipur
+
+MODY University
+
+Parul University
